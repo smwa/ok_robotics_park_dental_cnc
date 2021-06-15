3b601b54b05bb4328565ceb8c8b48b5615681cd0
Michael Smith <michael.smith.ok@gmail.com>
2021-06-15 15:20:06 -0500

    Revert splash change

--- install.sh
+++ install.sh
@@ -102,4 +102,4 @@ sudo echo "static ip_address=192.168.0.25" >> /etc/dhcpcd.conf
 sudo raspi-config nonint do_boot_wait 0
 
 # Disable Raspbian splash screen
-sudo raspi-config nonint do_boot_splash 0
+sudo raspi-config nonint do_boot_splash 1
