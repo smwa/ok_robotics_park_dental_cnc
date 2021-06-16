7c5efb544a0be5a9f0cc535f59fe878c940a9f22
Michael Smith <michael.smith.ok@gmail.com>
2021-06-16 10:25:47 -0500

    Quiet command

--- install.sh
+++ install.sh
@@ -78,7 +78,7 @@ sudo mkdir -p /media/pi/gcode
 sudo chown pi /media/pi/gcode
 sudo chgrp pi /media/pi/gcode
 sudo sed -i "/MilFiles/d" /etc/fstab
-echo "//192.168.0.15/MilFiles /media/pi/gcode cifs user,uid=1000,r,suid 0 0" | sudo tee -a /etc/fstab
+sudo sh -c 'echo "//192.168.0.15/MilFiles /media/pi/gcode cifs user,uid=1000,r,suid 0 0" >> /etc/fstab'
 
 # Clear networking
 sudo sed -i "/interface/d" /etc/dhcpcd.conf
